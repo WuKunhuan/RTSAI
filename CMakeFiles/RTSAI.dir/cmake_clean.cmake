d8889073b8ed2cd8cd24f31a900dc48400d072f2
WuKunhuan <u3577163@connect.hku.hk>
2023-11-08 10:26:58 +0800

    change README.md : update CMakeLists.txt

--- CMakeFiles/RTSAI.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "RTSAI"
-  "RTSAI.pdb"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/RTSAI.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
