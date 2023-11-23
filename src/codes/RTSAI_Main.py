524414dadb5709b1b8feac881808b04b572b715c
WuKunhuan <u3577163@connect.hku.hk>
2023-11-23 18:51:18 +0800

    [build] further add blueprint in main

--- src/codes/RTSAI_Main.py
+++ src/codes/RTSAI_Main.py
@@ -7,6 +7,8 @@ CURRENT_ENV = "default"
 RED = "\033[91m"
 RESET = "\033[0m"
 
+
+
 # 2. Functions
 
 ## This function gets the manual of the RTSAI command
@@ -27,10 +29,14 @@ def index_safe(l, id):
 def convert_to_red(text):
     return f"{RED}{text}{RESET}"
 
+
+
 # 3. The Main function
 
 ## The main function handles RTSAI commands
 def main():
+
+
 
     # 3.1 Handle the command : RTSAI
     ## This command display a Hello World message
@@ -39,7 +45,12 @@ def main():
             print("Welcome to the world of RTSAI!")
     else: return 0
 
+
+
     # 3.2 Handle the environment command : RTSAI env
+
+    ## An environment, is basically one set of contexts + pre-defined graphs
+    ## you could think it as one user's chatgpt account
     if (index_safe(sys.argv, 1) == 'env'):
 
         ## List all environments
@@ -67,20 +78,47 @@ def main():
             global CURRENT_ENV
             CURRENT_ENV = 'default'
     
+
+
     # 3.3 Handle the chatbot command : RTSAI chat
+
+    ## A chat, basically means generating some contexts as another (one or more) knowledge graphs
+    ## Context + Prior = the agent's knowledge. 
+    ## you could think it as one user's chatgpt chat
     elif (index_safe(sys.argv, 1) == 'chat'): 
 
-        ## --context : previous context provided
-        ## --graph : specific knowledge graph(s) provided
-
-        ## only one context can be specified
-        if ('--context' in sys.argv): 
+        ## List all existing chats
+        if (index_safe(sys.argv, 2) == 'list'): 
             pass
 
-        ## one or more graphs can be specified; 
-        ## regular expression selector will be supported
-        if ('--graph' in sys.argv): 
+        ## Delete a chat
+        elif (index_safe(sys.argv, 2) == 'delete'): 
             pass
+
+
+        else: 
+
+            ## --context : previous context provided
+            ## --graph : specific knowledge graph(s) provided
+
+            ## A name must be provided for the chat, cannot contain special characters, 
+            ## it will be modified by the 
+
+            ## only one context can be specified. 
+            ## taken from the previous stored chat. 
+            if ('--context' in sys.argv): 
+                pass
+
+            ## one or more graphs can be specified; 
+            ## regular expression selector will be supported
+            if ('--graph' in sys.argv): 
+                pass
+
+            ## specify this option to let the graphs able to be updated
+            ## the change is pernament, deemed to be the knowledge acquired by the agent
+            ## learn, may create new graphs, may merge graphs, etc. aiming to improve the topology
+            if ('--learn' in sys.argv): 
+                pass
 
     # 3.4 Handle the knowledge graph command : RTSAI graph
 
@@ -92,29 +130,64 @@ def main():
 
             ## can specify a graph to query, with -g option
 
-            ## must specify the query, with -q option, 
-            pass
-
-        ## graph visualize
-        if (index_safe(sys.argv, 2) == 'visualize'): 
-
-            ## must specify 
+            ## must specify the query, with -q option
             pass
 
         ## graph load
         if (index_safe(sys.argv, 2) == 'load'): 
 
-            ## specify the graph name, followed by the resource name, in csv
+            ## specify the graph name, followed by the resource name, in csv. 
             ## if the name exists, that means overriding
             ## if the name does not exist, that means overriding
             pass
 
+        ## graph combine
+        if (index_safe(sys.argv, 2) == 'combine'): 
+            ## combine several graphs together into a folder (instead of merge them)
+            pass
+
+        ## graph divide
+        if (index_safe(sys.argv, 2) == 'divide'): 
+            ## divide a graph (must be in folder form; merged before) into its parts
+            pass
+
         ## graph merge
+        if (index_safe(sys.argv, 2) == 'merge'): 
+            ## merge several CSV files, as specified. 
+            ## -  Done by the agent. Conflict detection and management will be conducted. 
+            ## -  an replacing flag "--replace" can be provided, to remove all previous graphs. 
+            pass
 
+        ## graph split
+        if (index_safe(sys.argv, 2) == 'split'): 
+            ## split a graph, automatically done by the agent. 
+            ## this graph must be created by merge, which allows it to be splitted accordingly
+            pass
 
-        ## graph save
+        ## graph feed
+        if (index_safe(sys.argv, 2) == 'feed'): 
+            ## feed a graph and extend it according to the natural language input
+            ## source of the feeding must be provided
+            pass
+
+        ## graph extract
+        if (index_safe(sys.argv, 2) == 'extract'): 
+            ## extract some knowlege to create a subgraph
+            ## A description, stored in a .txt file, must be provided
+            pass
 
         ## graph delete
+        if (index_safe(sys.argv, 2) == 'delete'): 
+            ## extract some knowlege to create a subgraph
+            ## A description, stored in a .txt file, must be provided
+            pass
+
+        ## graph visualize
+        if (index_safe(sys.argv, 2) == 'visualize'): 
+
+            ## must specify a graph to visualize
+            ## this function will be implemented via tkinter, if time allows
+            pass
 
 
     else:
